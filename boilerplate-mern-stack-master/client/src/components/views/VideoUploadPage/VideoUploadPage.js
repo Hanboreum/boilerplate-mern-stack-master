05fbfe39a1bfeea1cb61ec8a7cc9949d2437ef57
fullm <matoma1_@naver.com>
2023-10-19 00:04:12 +0900

    Savingtoafileserver
     파일 서버에 저장하기

--- boilerplate-mern-stack-master/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Typography, Button, Form, message, Input, Icon} from 'antd'
 import Dropzone from 'react-dropzone'
+import Axios from 'axios';
 //import TextArea from 'antd/lib/input/TextArea';
 
 const{TextArea} = Input;
@@ -42,6 +43,24 @@ function VideoUploadPage() {
       setCategory(e.currentTarget.value)
     }
 
+    const onDrop = (files) => {
+     let formDate  = new FormData;
+     const config = {
+      header: {'content-type': 'multipart/form-data'}
+     }
+     formDate.append("file",files[0])
+     
+     Axios.post('/api/video/uploadfiles',formDate, config)
+     .then(response => {
+      if(response.data.success){
+        console.log(response.data)
+
+      }else{
+        alert('비디오 업로드 실패.')
+      }
+     })
+    }
+
 
   return (
     <div style={{maxWidth:'700px' , margin:'2rem auto'}}>
@@ -55,9 +74,10 @@ function VideoUploadPage() {
                      {/**drop zone */}
 
                      <Dropzone
-                     onDrop
-                     multiple
-                     maxSize>
+                     onDrop = {onDrop}
+                     multiple ={false}
+                     maxSize ={1000000}
+                     >
                      {({ getRootProps, getInputProps}) =>(
                         <div style={{width:'300px', height:'240px', border:'1px solid lightgray', display :'flex',
                          alignItems:'center', justifyContent:'center'}}{...getRootProps()}>
