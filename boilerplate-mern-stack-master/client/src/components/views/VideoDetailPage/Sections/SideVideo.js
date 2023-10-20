50df2be52f272a7346ab7a8018c99cb4933c9f43
fullm <matoma1_@naver.com>
2023-10-20 16:27:48 +0900

    card template
    한개의 카드 템플릿 생성

--- boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/Sections/SideVideo.js
+++ boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/Sections/SideVideo.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 function SideVideo() {
   return (
-    <div>
-      side video
-    </div>
+    <div style={{ display: 'flex', marginTop: '1rem', padding: '0 2rem' }}>
+       <div style={{ width:'40%', marginRight:'1rem' }}>
+            <a href>
+                <img style={{ width: '100%' }} src alt/>
+            </a>    
+      </div>  
+      <div style={{ width:'50%' }}>
+            <a href>
+                <span style={{ fontSize: '1rem', color: 'black' }}>{video.title}  </span><br />
+                <span>{video.writer.name}</span><br />
+                <span>{video.views}</span><br />
+                <span>{minutes} : {seconds}</span><br />
+            </a>
+        </div>
+    </div> 
   )
 }
 
