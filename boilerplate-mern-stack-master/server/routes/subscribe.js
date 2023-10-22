15a4603df5edf72dae8f6af7509983d8de0e8076
fullm <matoma1_@naver.com>
2023-10-22 18:24:58 +0900

    Subscribed
    구독기능1

--- boilerplate-mern-stack-master/server/routes/subscribe.js
+++ boilerplate-mern-stack-master/server/routes/subscribe.js
@@ -17,23 +17,18 @@ router.post("/subscribeNumber", (req, res) => {
 
     Subscriber.find({ 'userTo': req.body.userTo })
     .exec((err, subscribe) => {
-        if(err) return res.status(400).send(err)
+        if(err) return res.status(400).send(err);
 
         return res.status(200).json({ success: true, subscribeNumber: subscribe.length  })
     })
 
-});
-router.post("/subscribed", (req, res) => {
-
-    
 });
 
 router.post("/subscribed", (req, res) => {
 
     Subscriber.find({ "userTo": req.body.userTo , "userFrom": req.body.userFrom })
     .exec((err, subscribe) => {
-        if(err) return res.status(400).send(err)
-
+        if(err) return res.status(400).send(err);
         let result = false;
         if(subscribe.length !== 0) {
             result = true //구독하고있음
