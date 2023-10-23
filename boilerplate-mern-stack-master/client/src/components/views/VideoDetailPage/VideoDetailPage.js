6fd83acdb60d8fa3322017bae1d5599e74f44555
fullm <matoma1_@naver.com>
2023-10-23 13:44:07 +0900

    VideoDetailpage
    해당 페이지에 대한 writer 에러 해결

--- boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -4,12 +4,11 @@ import axios from 'axios';
 import SideVideo from './Sections/SideVideo';
 import Subscribe from './Sections/Subscribe';
 
-
-
 function VideoDetailPage(props) {
 
     const videoId = props.match.params.videoId
     const variable ={ videoId:videoId }
+    const [Video, setVideo] = useState([])
     const [VideoDetail, setVideoDetail] = useState([])
 
     
@@ -17,8 +16,8 @@ function VideoDetailPage(props) {
         axios.post('/api/video/getVideoDetail', variable)
             .then(response => {
                 if (response.data.success) {
-                    console.log(response.data.video)
-                    setVideoDetail(response.data.VideoDetail)
+                    console.log(response.data.videoDetail) //video
+                    setVideoDetail(response.data.videoDetail) //VideoDetail
                 } else {
                     alert('비디오 정보를 가져오는데 실패했습니다. ')
                 }
@@ -26,16 +25,20 @@ function VideoDetailPage(props) {
             
         },[])
             
-        if(VideoDetail.writer){
+        if(VideoDetail.writer) {
+            console.log(VideoDetail)
 
+            const subscribeButton = VideoDetail.writer._id !== localStorage.getItem('userId')&& <Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')} />
+            
             return (
                 <Row gutter ={[16,16]}>
                 <Col lg ={18} xs = {24}>
+
                     <div style={{width:'100%', padding:'3rem 4rem'}}>
                         <video style={{width: '100%'}} src={`http://localhost:5000/${VideoDetail.filePath}`} controls/>
             
                         <List.Item
-                        actions ={[<Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')} />]} 
+                          actions ={[subscribeButton]} 
                         >
                          <List.Item.Meta
                              avatar={<Avatar src={VideoDetail.writer.image} />}
